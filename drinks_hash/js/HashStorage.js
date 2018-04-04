55ea1cf535076946f78150173611a1c92c67cc43
Sergey Karpovich <wxvwvxw@gmail.com>
2018-04-04 21:15:11 +0300

    +

--- drinks_hash/js/HashStorage.js
+++ drinks_hash/js/HashStorage.js
@@ -12,7 +12,7 @@
             return this.storage[key];
         }
         else {
-            return undefined;
+            return undefined; 
         }
     }
     HashStorage.prototype.deleteValue = function(key) {
